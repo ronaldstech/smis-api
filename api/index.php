4e46ad43e2906a1d6c7d710f0b4bd6770d127761
Jacob Liwewe <liwewejacob265@gmail.com>
2026-01-08 19:11:25 +0200

    udated cors

--- api/index.php
+++ api/index.php
@@ -1,8 +1,19 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+$allowedOrigins = [
+    'https://unimarket-mw.com',
+    'http://localhost:5173',
+    'http://localhost:3000'
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+    header("Access-Control-Allow-Credentials: true");
+}
+
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
+
 
 // Handle preflight requests
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
